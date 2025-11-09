ec996868dcadfacb168f923ee6f9d97c6be5b276
git-sandoval98 <sergioa.munoz@udla.edu.co>
2025-11-09 04:50:15 -0500

    arreglos de claves

--- src/Firebase/ConfigFirebase.js
+++ src/Firebase/ConfigFirebase.js
@@ -1,22 +1,30 @@
+// src/Firebase/ConfigFirebase.js
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
-// import { getStorage } from "firebase/storage"; // despues lo descomento cuando active Storage
+// import { getStorage } from "firebase/storage"; // cuando lo actives
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDHCj-Tn78asKoIC-aNHqJJsqeEcKuspdc",
-  authDomain: "pfg2-munozsergio-ramossantiago.firebaseapp.com",
-  projectId: "pfg2-munozsergio-ramossantiago",
-  storageBucket: "pfg2-munozsergio-ramossantiago.firebasestorage.app",
-  messagingSenderId: "808626790905",
-  appId: "1:808626790905:web:053cd40f1079b251ecdd28"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+// export const storage = getStorage(app);
 
-// export const storage = getStorage(app); // esto tambien lo descomento despues de activar Storage
 
-export default app;
+
+
+
+ // apiKey: "AIzaSyDHCj-Tn78asKoIC-aNHqJJsqeEcKuspdc",
+ // authDomain: "pfg2-munozsergio-ramossantiago.firebaseapp.com",
+  //projectId: "pfg2-munozsergio-ramossantiago",
+  //storageBucket: "pfg2-munozsergio-ramossantiago.firebasestorage.app",
+  //messagingSenderId: "808626790905",
+  //appId: "1:808626790905:web:053cd40f1079b251ecdd28"
